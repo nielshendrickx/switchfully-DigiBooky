08d7d232f52f89f527adfc13d9928eacc4787464
nielsh <Niemand@01>
2020-03-25 21:23:17 +0100

    Initial Skeleton

---
+++ api/src/main/java/com/switchfully/javadocjuveniles/api/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,11 @@
+package com.switchfully.javadocjuveniles.api.exceptions;
+
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+@ControllerAdvice
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+
+}
