e1d6cc248874abe68fa5a10414eb1a8a7fc5a048
nielsh <Niemand@01>
2020-03-26 14:26:21 +0100

    Removed public capsulation from interface

--- domain/src/main/java/com/switchfully/javadocjuveniles/domain/book/Borrowable.java
+++ domain/src/main/java/com/switchfully/javadocjuveniles/domain/book/Borrowable.java
@@ -4,14 +4,14 @@ import java.util.Date;
 
 public interface Borrowable {
 
-    public String getTitle();
-    public String getSummary();
-    public int getNumberOfCopies();
-    public Date getDateAdded();
-    public void setTitle(String title);
-    public void setSummary(String summary);
-    public void setNumberOfCopies(int numberOfCopies);
-    public void toggleAvailability();
-    public String getDetails();
+    String getTitle();
+    String getSummary();
+    int getNumberOfCopies();
+    Date getDateAdded();
+    void setTitle(String title);
+    void setSummary(String summary);
+    void setNumberOfCopies(int numberOfCopies);
+    void toggleAvailability();
+    String getDetails();
 
 }
