9ad894d3e5ce7d243b013136dcf2cd1c73bb04c0
Sven De Potter <sven.depotter@hotmail.com>
2020-03-26 20:29:18 +0100

    test Validation

--- api/src/test/java/com/switchfully/javadocjuveniles/api/security/validation/ValidationTest.java
+++ api/src/test/java/com/switchfully/javadocjuveniles/api/security/validation/ValidationTest.java
@@ -1,10 +1,8 @@
 package com.switchfully.javadocjuveniles.api.security.validation;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class ValidationTest {
     @Test
