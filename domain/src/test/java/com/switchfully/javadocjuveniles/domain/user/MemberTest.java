0f347a882d37ae97201508f5ea3b5c3ca9d3be3f
svendp1988 <56538162+svendp1988@users.noreply.github.com>
2020-03-29 13:30:00 +0200

    more tests, tests, tests

--- domain/src/test/java/com/switchfully/javadocjuveniles/domain/user/MemberTest.java
+++ domain/src/test/java/com/switchfully/javadocjuveniles/domain/user/MemberTest.java
@@ -5,27 +5,35 @@ import org.junit.jupiter.api.Test;
 
 import static com.switchfully.javadocjuveniles.domain.user.builders.AddressBuilder.addressBuilder;
 import static com.switchfully.javadocjuveniles.domain.user.builders.UserBuilder.userBuilder;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class MemberTest {
 
     @Test
     void createMember() {
+        Address address = addressBuilder()
+                .withCity("Brussels")
+                .withPostalCode("1000")
+                .withStreet("Piglane")
+                .withStreetNumber("42")
+                .build();
         Member member = userBuilder()
                 .withINSS("299.18.02-10.08.1996")
-                .setAddress(addressBuilder()
-                        .withCity("Brussels")
-                        .withPostalCode("1000")
-                        .withStreet("Piglane")
-                        .withStreetNumber("42")
-                        .build()
-                )
+                .setAddress(address)
                 .withFirstName("FirstName")
                 .withLastName("LastName")
                 .withEmail("test@email.com")
                 .withPassWord("aPassword")
                 .buildMember();
 
-        Assertions.assertEquals("299.18.02-10.08.1996", member.getINSS());
+        assertEquals("299.18.02-10.08.1996", member.getINSS());
+        assertEquals(address, member.getAddress());
+        assertEquals("FirstName", member.getFirstName());
+        assertEquals("LastName", member.getLastName());
+        assertEquals("test@email.com", member.getEmail());
+        assertEquals("aPassword", member.getPassword());
+        assertNotNull(member.getId());
     }
 
 }
--- domain/src/test/java/com/switchfully/javadocjuveniles/domain/user/UserTest.java
+++ domain/src/test/java/com/switchfully/javadocjuveniles/domain/user/UserTest.java
@@ -1,14 +1,29 @@
 
 package com.switchfully.javadocjuveniles.domain.user;
 
+import com.switchfully.javadocjuveniles.domain.user.feature.UserRole;
 import org.junit.jupiter.api.Test;
 
+import static com.switchfully.javadocjuveniles.domain.user.builders.UserBuilder.userBuilder;
+import static com.switchfully.javadocjuveniles.domain.user.feature.UserRole.ADMIN;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
 
     @Test
-    void createUser() {
-
+    void createUser_shouldReturnCorrectValues() {
+        User user = userBuilder()
+                .withFirstName("firstName")
+                .withLastName("lastName")
+                .withEmail("email")
+                .withRole(ADMIN)
+                .withPassWord("password")
+                .buildUser();
+        assertEquals("firstName", user.getFirstName());
+        assertEquals("lastName", user.getLastName());
+        assertEquals("email", user.getEmail());
+        assertEquals(ADMIN, user.getRole());
+        assertEquals("password", user.getPassWord());
+        assertNotNull(user.getId());
     }
 }
