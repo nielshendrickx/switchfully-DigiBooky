76e7bc5d1489b971f609a41a6d9a3863eafeb85c
FitzSilk <60465804+FitzSilk@users.noreply.github.com>
2020-03-31 12:50:03 +0200

    Create dummy data

--- domain/src/main/java/com/switchfully/javadocjuveniles/domain/DummyData.java
+++ domain/src/main/java/com/switchfully/javadocjuveniles/domain/DummyData.java
@@ -36,8 +36,83 @@ public class DummyData {
 
         defaultBooks.add(book);
 
+        Book book1 = bookBuilder()
+                .withAuthor(authorBuilder()
+                        .withFirstName("Robert")
+                        .withLastName("Martin")
+                        .build()
+                )
+                .withInitialPrice(15)
+                .withISBN("9781670066879")
+                .withNumberOfCopies(1)
+                .withSummary("Summary of a wonderful book")
+                .withTitle("Clean Code: A Handbook for Beginners to Learn How to Become a Better Programmer")
+                .build();
+
+        defaultBooks.add(book1);
+
+        Book book2 = bookBuilder()
+                .withAuthor(authorBuilder()
+                        .withFirstName("Robert")
+                        .withLastName("Martin")
+                        .build()
+                )
+                .withInitialPrice(15)
+                .withISBN("9780137081073")
+                .withNumberOfCopies(1)
+                .withSummary("Summary of a wonderful book")
+                .withTitle("The Clean Coder: A Code of Conduct for Professional Programmers")
+                .build();
+
+        defaultBooks.add(book2);
+
+        Book book3 = bookBuilder()
+                .withAuthor(authorBuilder()
+                        .withFirstName("J.K.")
+                        .withLastName("Rowling")
+                        .build()
+                )
+                .withInitialPrice(7)
+                .withISBN("9780439785969")
+                .withNumberOfCopies(1)
+                .withSummary("Summary of a wonderful book")
+                .withTitle("Harry Potter and the Half-Blood Prince (Book 6)")
+                .build();
+
+        defaultBooks.add(book3);
+
+        Book book4 = bookBuilder()
+                .withAuthor(authorBuilder()
+                        .withFirstName("George")
+                        .withLastName("Orwell")
+                        .build()
+                )
+                .withInitialPrice(5)
+                .withISBN("9780451524935")
+                .withNumberOfCopies(1)
+                .withSummary("Everyone should have read this.")
+                .withTitle("1984")
+                .build();
+
+        defaultBooks.add(book4);
+
+        Book book5 = bookBuilder()
+                .withAuthor(authorBuilder()
+                        .withFirstName("Stephen")
+                        .withLastName("King")
+                        .build()
+                )
+                .withInitialPrice(5)
+                .withISBN("9781501142970")
+                .withNumberOfCopies(1)
+                .withSummary("A well done summary.")
+                .withTitle("It")
+                .build();
+
+        defaultBooks.add(book5);
+
         Member member = userBuilder()
-                .withFirstName("Eric")
+                .withFirstName("John")
                 .withLastName("Doe")
                 .withPassWord("DEAD")
                 .withINSS("10.10.10-101.10")
@@ -53,10 +128,45 @@ public class DummyData {
 
         defaultMembers.add(member);
 
+        Member member1 = userBuilder()
+                .withFirstName("Eric")
+                .withLastName("Schmidt")
+                .withPassWord("root")
+                .withINSS("10.10.10-101.10")
+                .withEmail("eric.schmidt@fun.com")
+                .withRole(UserRole.MEMBER)
+                .setAddress(addressBuilder()
+                        .withStreetNumber("12")
+                        .withStreet("Cowlane")
+                        .withPostalCode("1030")
+                        .withCity("Schaerbeek")
+                        .build())
+                .buildMember();
+
+        defaultMembers.add(member1);
+
+        Member member2 = userBuilder()
+                .withFirstName("Jane")
+                .withLastName("Doe")
+                .withPassWord("DEAD")
+                .withINSS("10.10.10-101.10")
+                .withEmail("jane.doe@dead.com")
+                .withRole(UserRole.MEMBER)
+                .setAddress(addressBuilder()
+                        .withStreetNumber("42")
+                        .withStreet("Piglane")
+                        .withPostalCode("1030")
+                        .withCity("Schaerbeek")
+                        .build())
+                .buildMember();
+
+        defaultMembers.add(member2);
+
         Borrow borrow = Borrow.BorrowBuilder.borrowBuilder()
                 .withBorrowable(book)
+                .setDummyDate()
                 .withMember(member)
-                .buildOverdueBorrow();
+                .buildBorrow();
         defaultBorrows.add(borrow);
     }
 
